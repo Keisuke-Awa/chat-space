811878e3d57b27a8d3353d184c251d3daae8469a
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-20 12:18:37 +0900

    user用seed.rbの追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  User.create(name: 'keisuke',email: 'keisuke.awa.basket@gmail.com', password: 'keisuke1027')
