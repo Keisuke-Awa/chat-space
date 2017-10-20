8cc30dccfb8272ef1de8149790f8c736124f48ae
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-20 13:57:56 +0900

    userのseeds文の削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-  User.create(name: 'keisuke',email: 'keisuke.awa.basket@gmail.com', password: 'keisuke1027')
