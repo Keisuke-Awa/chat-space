26a13e03b1e524a84fbe21107403d3eb55ed332f
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-21 10:40:32 +0900

    ユーザ情報編集後リダイレクト先の指定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,11 @@ class UsersController < ApplicationController
 
   def update
     current_user.update(update_params)
+    redirect_to root_path
   end
 
   private
   def update_params
-    params(:user).permit(:name, :email)
+    params[:user].permit(:name, :email)
   end
 end
