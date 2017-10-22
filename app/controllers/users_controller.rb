1e6056a28ae49c4ea2e605d4a6897b3fdda1d248
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-22 17:15:00 +0900

    users_controllerにてset_userのメソッドを作成、update/editアクションを修正・簡略化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    current_user.update(update_params)
+    @user.update(update_params)
     redirect_to root_path
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def update_params
     params[:user].permit(:name, :email)
   end
