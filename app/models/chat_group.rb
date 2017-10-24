5f4c451d2321f62004986ea33662e603b9bbee1d
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-24 21:49:43 +0900

    アソシエーション記述位置の修正

--- app/models/chat_group.rb
+++ app/models/chat_group.rb
@@ -9,7 +9,7 @@
 #
 
 class ChatGroup < ApplicationRecord
-  has_many :users, through: :chat_group_users
   has_many :chat_group_users
+  has_many :users, through: :chat_group_users
   accepts_nested_attributes_for :chat_group_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :chat_groups, through: :chat_group_users
   has_many :chat_group_users
+  has_many :chat_groups, through: :chat_group_users
 end
