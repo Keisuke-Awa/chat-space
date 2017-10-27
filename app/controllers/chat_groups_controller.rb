95a49589eedfad73e1a590c482a15ed52803b993
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-28 02:04:57 +0900

    createアクションによる条件分岐

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -4,10 +4,24 @@ class ChatGroupsController < ApplicationController
     @chat_group = ChatGroup.new
   end
 
-  def create; end
+  def create
+    chat_group = ChatGroup.new(chat_group_params)
+    respond_to do |format|
+      if chat_group.save
+        format.html { redirect_to root_path, notice: 'グループを作成しました' }
+      else
+        format.html { redirect_to root_path, alert: '作成に失敗しました' }
+      end
+    end
+  end
 
   def edit; end
 
   def update; end
+
+  private
+  def chat_group_params
+    params.require(:chat_group).permit(:name, user_ids: [] )
+  end
 
 end
