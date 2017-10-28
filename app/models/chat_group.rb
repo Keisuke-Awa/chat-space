ab18f095f25df4d1164458582efe0ae20b4fd6d7
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-29 02:16:01 +0900

    nameのvalidationを記述

--- app/models/chat_group.rb
+++ app/models/chat_group.rb
@@ -12,4 +12,7 @@ class ChatGroup < ApplicationRecord
   has_many :chat_group_users
   has_many :users, through: :chat_group_users
   accepts_nested_attributes_for :users, allow_destroy: true
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
