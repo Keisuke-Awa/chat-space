b220ded8f2890fdb6b613df4ff72a5cd00f3da4e
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-29 01:43:08 +0900

    accepts_nested_attributes_forにわたすモデルが間違っていたので修正

--- app/models/chat_group.rb
+++ app/models/chat_group.rb
@@ -11,5 +11,5 @@
 class ChatGroup < ApplicationRecord
   has_many :chat_group_users
   has_many :users, through: :chat_group_users
-  accepts_nested_attributes_for :chat_group_users
+  accepts_nested_attributes_for :users, allow_destroy: true
 end
