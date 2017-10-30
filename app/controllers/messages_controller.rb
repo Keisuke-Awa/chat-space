280729575277938aa3b510c64aee6156182dcaf8
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-30 18:27:52 +0900

    グループ一覧をdecorateした

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    @chat_groups = current_user.chat_groups
+    @chat_groups = ChatGroupDecorator.decorate_collection(current_user.chat_groups)
   end
 
 end
