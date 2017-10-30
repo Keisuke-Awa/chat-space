0b2f9a4e45308f302100f1a49816717b63a98dfe
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-31 00:31:24 +0900

    decoratorの修正

--- app/decorators/chat_group_decorator.rb
+++ app/decorators/chat_group_decorator.rb
@@ -6,10 +6,10 @@ class ChatGroupDecorator < Draper::Decorator
   end
 
   def errors_count
-    "#{ object.errors.count }件のエラーが発生しました。"
+    object.errors.count
   end
 
-  def full_messages
+  def error_messages
     object.errors.full_messages
   end
 
