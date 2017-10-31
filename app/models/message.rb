dc557428501665ec38ad946b4d0edd14c6d7bf8e
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-31 21:21:29 +0900

    messageモデルにおいて、body/imageのいずれかが必要になるバリデーションを作成

--- app/models/message.rb
+++ app/models/message.rb
@@ -14,4 +14,11 @@
 class Message < ApplicationRecord
   belongs_to :chat_group
   belongs_to :user
+
+  validates :body_or_image, presence: true
+
+  private
+  def body_or_image
+    body.presence or image.presence
+  end
 end
