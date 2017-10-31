595fd748227edcbd4202eac7bc6a1441cb366fee
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-10-31 20:57:28 +0900

    message.rbにannotateを実行

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id            :integer          not null, primary key
+#  body          :text(65535)      not null
+#  image         :string(255)
+#  user_id       :integer
+#  chat_group_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Message < ApplicationRecord
   belongs_to :chat_group
   belongs_to :user
