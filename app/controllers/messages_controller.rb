e52a0d3abad8ed0110ea03bf8c6708edc6ab4039
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-11-03 01:42:31 +0900

    indexアクションに@messagesを追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,14 +3,15 @@ class MessagesController < ApplicationController
   before_action :set_chat_group, only: [:index, :create]
 
   def index
-    @chat_groups = ChatGroupDecorator.decorate_collection(current_user.chat_groups)
+    @messages = @chat_group.messages.includes(:user).order('messages.id ASC')
     @message = Message.new
+    @chat_groups = ChatGroupDecorator.decorate_collection(current_user.chat_groups)
   end
 
   def create
     @message = Message.new(message_params)
     if @message.save
-      @messages = @chat_group.messages.includes(:user).order('message.id ASC')
+      @messages = @chat_group.messages.includes(:user).order('messages.id ASC')
       redirect_to chat_group_messages_path(@chat_group), notice: 'メッセージが作成されました'
     else
       redirect_to chat_group_messages_path(@chat_group), alert: 'メッセージの作成に失敗しました'
