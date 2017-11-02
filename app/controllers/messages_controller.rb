3674060541a447a24b124426fbe938608320095a
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-11-03 01:12:39 +0900

    messages#createの作成とストロングパラメータの定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,10 +7,20 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
-  def create; end
+  def create
+    @message = Message.new(message_params).decorate
+    if @message.save
+      @messages = @chat_group.messages.includes(:user).order('message.id ASC')
+      redirect_to chat_group_messages_path(@chat_group), notice: 'メッセージが作成されました'
+    else
+      redirect_to chat_group_messages_path(@chat_group), alert: @message.error_messages
+    end
+  end
 
   private
-  def message_params; end
+  def message_params
+    params.require(:message).permit(:body, :image).merge(chat_group_id: params[:chat_group_id], user_id: current_user.id)
+  end
 
 end
 
