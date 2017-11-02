5c731f823500645d7c27b8742eee31ebe3f2242b
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-11-03 00:52:29 +0900

    messages#indexの定義・インスタンスの設置

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,9 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
 
   def index
+    @chat_group = ChatGroup.find(params[:chat_group_id])
     @chat_groups = ChatGroupDecorator.decorate_collection(current_user.chat_groups)
+    @message = Message.new
   end
 
   def create; end
