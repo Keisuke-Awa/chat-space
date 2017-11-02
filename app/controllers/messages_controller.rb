aa630ed88f0cbe4f63403ab2aa343335fa159b3e
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-11-03 01:17:15 +0900

    messageのdecorateを一旦全て外した（定義していなかったため）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,18 +8,18 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params).decorate
+    @message = Message.new(message_params)
     if @message.save
       @messages = @chat_group.messages.includes(:user).order('message.id ASC')
       redirect_to chat_group_messages_path(@chat_group), notice: 'メッセージが作成されました'
     else
-      redirect_to chat_group_messages_path(@chat_group), alert: @message.error_messages
+      redirect_to chat_group_messages_path(@chat_group), alert: 'メッセージの作成に失敗しました'
     end
   end
 
   private
   def set_chat_group
-    @chat_group = ChatGroup.find(params[:chat_group_id]).decorate
+    @chat_group = ChatGroup.find(params[:chat_group_id])
   end
 
   def message_params
