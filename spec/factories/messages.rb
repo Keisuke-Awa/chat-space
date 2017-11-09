30c82e48322a0f9f31d39ef0aa4b51e416c0099f
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-11-09 19:37:11 +0900

    アソシエーションの設定を追加した

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,10 +3,10 @@ require 'faker'
 FactoryBot.define do
 
   factory :message do
-    user_id           rand(1..3),
-    chat_group_id     rand(1..3),
-    body              Faker::HarryPotter.quote,
-    image             Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/factories/images/ringo.jpg'))
+    association :user,       factory: :user
+    association :chat_group, factory: :chat_group
+    body              { Faker::HarryPotter.quote }
+    image             { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/factories/fixtures/ringo.jpg')) }
   end
 
 end
