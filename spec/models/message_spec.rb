11de2c3012dbb03dd9148b3538dc4fe316d5f25a
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-11-09 19:48:43 +0900

    画像と文章がない場合のテストを通過

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,18 +2,25 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
 
-    it "is valid with a body" do
+    it "is valid with a body, user_id, chat_group_id" do
       message = build(:message, image: nil)
       expect(message).to be_valid
     end
 
-    it "is valid with a body" do
+    it "is valid with a image, user_id, chat_group_id" do
+      message = build(:message, body: nil)
+      expect(message).to be_valid
     end
 
-    it "is valid with a body" do
+    it "is valid with a body, image, user_id, chat_group_id" do
+      message = build(:message)
+      expect(message).to be_valid
     end
 
-    it "is valid with a body" do
+    it "is invalid without a body and image" do
+      user = build(:message, body: "", image: "")
+      user.valid?
+      expect(user.errors[:body_or_image]).to include("を入力してください")
     end
 
   end
