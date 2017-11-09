8258b5634038209d18d97abe2284cc11d3bd2a4d
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2017-11-09 19:37:37 +0900

    bodyだけの場合のテストを通過

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 describe Message do
   describe '#create' do
-    it "is invalid without a nickname" do
+
+    it "is valid with a body" do
+      message = build(:message, image: nil)
+      expect(message).to be_valid
     end
+
+    it "is valid with a body" do
+    end
+
+    it "is valid with a body" do
+    end
+
+    it "is valid with a body" do
+    end
+
   end
 end
