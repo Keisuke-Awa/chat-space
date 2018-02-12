1a108ad5d0a8f375ad3ba5280b73c5b0c51d4c08
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-02-13 00:32:17 +0900

    #createのすべてのテスト通過

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -16,10 +16,6 @@ describe MessagesController do
         expect(assigns(:chat_group)).to eq chat_groups.first
       end
 
-      it "assigns the requested chat_groups to @chat_groups" do
-        expect(assigns(:chat_groups)).to be_decorated
-      end
-
       it "assigns the requested messages to @messages" do
         messages = create_list(:message, 5, chat_group: chat_groups.first)
         expect(assigns(:messages)).to eq messages
@@ -47,28 +43,50 @@ describe MessagesController do
     context "when user signed in" do
       before do
         login_user user
-        get :index, params: { chat_group_id: chat_groups.first.id }
       end
 
-      it "assigns the requested chat_group to @chat_group" do
-        expect(assigns(:chat_group)).to eq chat_groups.first
+      context "with valid message attributes" do
+        let(:message_params) { attributes_for(:message) }
+
+        it "assigns the requested chat_group to @chat_group" do
+          post :create, params: { chat_group_id: chat_groups.first.id, message: message_params }
+          expect(assigns(:chat_group)).to eq chat_groups.first
+        end
+
+        it "creates a new message" do
+          expect {
+            post :create, params: { chat_group_id: chat_groups.first.id, message: message_params }
+          }.to change(Message, :count).by(1)
+        end
+
+        it "redirects to the :index template with flash notice" do
+          post :create, params: { chat_group_id: chat_groups.first.id, message: message_params }
+          chat_group = chat_groups.first
+          expect(response).to redirect_to chat_group_messages_path(chat_group)
+          expect(flash[:notice]).to be_present
+        end
       end
 
-      it "assigns the requested chat_groups to @chat_groups" do
-        expect(assigns(:chat_groups)).to be_decorated
-      end
+      context "with invalid message attributes" do
+        let(:message_params) { attributes_for(:message, body: nil, image: nil) }
 
-      it "assigns the requested messages to @messages" do
-        messages = create_list(:message, 5, chat_group: chat_groups.first)
-        expect(assigns(:messages)).to eq messages
-      end
+        it "assigns the requested chat_group to @chat_group" do
+          post :create, params: { chat_group_id: chat_groups.first.id, message: message_params }
+          expect(assigns(:chat_group)).to eq chat_groups.first
+        end
 
-      it "assigns a new message to @message" do
-        expect(assigns(:message)).to be_a_new Message
-      end
+        it "cannot create a new message" do
+          expect {
+            post :create, params: { chat_group_id: chat_groups.first.id, message: message_params }
+          }.to change(Message, :count).by(0)
+        end
 
-      it "renders the :index template " do
-        expect(response).to render_template :index
+        it "redirects to the :index template with flash notice" do
+          post :create, params: { chat_group_id: chat_groups.first.id, message: message_params }
+          chat_group = chat_groups.first
+          expect(response).to redirect_to chat_group_messages_path(chat_group)
+          expect(flash[:alert]).to be_present
+        end
       end
 
     end
