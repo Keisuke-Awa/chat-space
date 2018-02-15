fb955e5963ab976a5cc074037f070b6f4bbcabee
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-02-15 23:05:46 +0900

    chat_group_users.rbにschema記述

--- spec/factories/chat_group_users.rb
+++ spec/factories/chat_group_users.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: chat_group_users
+#
+#  id            :integer          not null, primary key
+#  user_id       :integer
+#  chat_group_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 FactoryBot.define do
 
   factory :chat_group_user do
