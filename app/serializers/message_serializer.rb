508f83e9ae6536bbc02a2793ae2ec29cc8af4e6f
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-02-17 21:55:17 +0900

    attributesを追加

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,5 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :body, :image, :user_id, :chat_group_id
+  attributes :id, :body, :image, :user_id, :chat_group_id, :created_at
   has_one :user
   has_one :chat_group
 end
