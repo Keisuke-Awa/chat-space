ed5c2ae3e44edbad4930fe23cdd1bb5bc6a01223
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-02-17 21:56:13 +0900

    ajaxでのメッセージの自動追加完了

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,10 +1,21 @@
 $(function(){
+    function buildHTML(message){
+        var html = `<div class='main-content__chat-area__message'>
+                      <div class='main-content__chat-area__message__info clearfix'>
+                        <h3>${ message.user.name }</h3>
+                        <p>${ message.created_at }</p>
+                      </div>
+                      <div class='main-content__chat-area__message__text'>
+                        <p>${ message.body }</p>
+                      </div>
+                    </div>`
+            return html;
+        }
     $('#new_message').on('submit', function(e){
         e.preventDefault();
         alert('hello');
         var formData = new FormData(this);
         var url = $(this).attr('action');
-        console.log(formData);
         $.ajax({
             url: url,
             type: "POST",
@@ -12,6 +23,12 @@ $(function(){
             dataType: 'json',
             processData: false,
             contentType: false
+        })
+        .done(function(data){
+            var html = buildHTML(data);
+            console.log(html);
+            $('.main-content__chat-area').append(html)
+            // $('.textbox').val('')
         })
     })
 })
