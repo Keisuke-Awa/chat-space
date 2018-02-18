f2ca09cb95eec176c734bf27411f89e0181c4e73
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-02-18 16:09:51 +0900

    strftimeを使ってcreated_atがフォーマット表示されるようにした

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,4 +1,9 @@
 class MessageSerializer < ActiveModel::Serializer
   attributes :id, :body, :image, :user_id, :created_at
   has_one :user
+
+  def created_at
+    object.created_at.strftime('%Y年%m月%d日 %H:%M')
+  end
+
 end
