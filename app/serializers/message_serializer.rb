2f7d884bfdfac1dc55e6c9f782eb4d793049e91b
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-02-20 08:20:09 +0900

    メソッドを用意

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,9 +1,17 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes  :body, :image, :created_at
+  attributes  :body, :created_at, :created_at_time, :image_url
   has_one :user
 
-  def created_at
+  def created_at_time
     object.created_at.strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def image_url
+    object.image&.url.to_s
+  end
+
+  def user_name
+    object.user&.name.to_s
   end
 
 end
