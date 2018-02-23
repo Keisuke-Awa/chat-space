fda92682d00a794de693dcb9256b85abf78f7936
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-02-23 09:49:15 +0900

    パラメータを仮置き

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update]
 
   def index
-    @users = User.where('name like ?', "%#{params[:keyword]}%").limit(10)
+    @users = User.where('name like ?', "%#{params[:name]}%").limit(10)
     respond_to do |format|
       format.html
       format.json
