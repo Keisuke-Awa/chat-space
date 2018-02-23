a2a96fb2aa69b5af22985ee56c6ea030b267ac97
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-02-23 09:38:15 +0900

    users#indexを追加（まだ空）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
   before_action :set_user, only: [:edit, :update]
 
+  def index; end
+
   def edit; end
 
   def update
