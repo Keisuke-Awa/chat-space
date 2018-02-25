1f4119aa6429e3fb4e32646d03c8ab7f4cee6430
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-02-25 17:14:47 +0900

    検索一覧のボタンにobjectを格納

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,34 +8,48 @@ $(function(){
     User.prototype.searchFieldHTML = function() {
         var html = '<div class="chat-group-user">';
         html += '<p class="chat-group-user__name">' + this.name + '</p>';
-        html += '<button class="chat-group-user__add-user-btn" type="button">追加</button>';
+        html += '<button class="chat-group-user__add-user-btn" type="button" data-obj="' + this + '">追加</button>';
         html += '</div>';
         return html;
     };
 
-    $(function() {
-        $('#user-search-field').on('keyup', function() {
-            $('#user-search-result').empty();
-            const input = $(this).val();
-            if (!input) {
-                return;
-            }
-            $.ajax({
-                type: 'GET',
-                url: '/users',
-                data: { query: input },
-                dataType: 'json'
-            })
-            .done(function(users) {
-                users.forEach( function( user ) {
-                    const chatGroupUser = new User(user);
-                    const html = chatGroupUser.searchFieldHTML()
-                    $('#user-search-result').append(html);
-                });
-            })
-            .fail(function(errors) {
-                alert(errors.responseJSON['errors']);
+    User.prototype.addMemberHTML = function() {
+        var html = '<div class="chat-group-user">';
+        html += '<p class="chat-group-user__name">' + this.name + '</p>';
+        html += '<button class="chat-group-user__delete-user-btn" type="button">削除</button>';
+        html += '<input name="chat_group[user_ids][]" type="hidden" value="' + this.id + '">';
+        html += '</div>';
+        return html;
+    };
+
+    User.prototype.deleteMemberHTML = function() {
+
+    };
+
+    $('#user-search-field').on('keyup', function() {
+        $('#user-search-result').empty();
+        const input = $(this).val();
+        if (!input) {
+            return;
+        }
+        $.ajax({
+            type: 'GET',
+            url: '/users',
+            data: { query: input },
+            dataType: 'json'
+        })
+        .done(function(users) {
+            users.forEach( function( user ) {
+                const chatGroupUser = new User(user);
+                const html = chatGroupUser.searchFieldHTML();
+                $('#user-search-result').append(html);
             });
+        })
+        .fail(function(errors) {
+            alert(errors.responseJSON['errors']);
         });
+    });
+
+    $(document).on("click", ".chat-group-user__add-user-btn", function(){
     });
 })
